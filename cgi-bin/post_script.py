319d1d34ba88f601ceb84ea7dfb1377d91a8ee63
Jongho Park <jonghopa@cx1r5s3.42seoul.kr>
2024-08-25 18:41:59 +0900

    POST Request CGI Script File is making

--- cgi-bin/post_script.py
+++ cgi-bin/post_script.py
@@ -6,5 +6,26 @@ import cgi
 import cgitb
 import os
 
-# 디버깅을 위한 설정
+# 에러 로깅 활성화
 cgitb.enable()
+
+# 폼 데이터 가져오기
+form = cgi.FieldStorage()
+name = form.getvalue('name')
+age = form.getvalue('age')
+
+# HTML 응답 생성
+print("Content-type: text/html\r\n", end="")
+print("<html>")
+print("<head>")
+print("<title>Form Response</title>")
+print("</head>")
+print("<body>")
+print("<h1>Form Submitted</h1>")
+if name and age:
+    print(f"<p>Name: {name}</p>")
+    print(f"<p>Age: {age}</p>")
+else:
+    exit(1)
+print("</body>")
+print("</html>")
