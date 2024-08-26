4193b50412f49d01d882229ad0cfb8763050141b
jhdgo1225 <parkjh99365@gmail.com>
2024-08-26 22:24:16 +0900

    application/json Content of CGI POST Request is Processing Very Well

--- test.py
+++ test.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
-import cgi
-import cgitb
+import json
 import os
 
-fp = open("/Users/devpark/Desktop/abc.png", 'rb')
-fp2 = open("tempContent1", 'wb')
-for line in fp.readlines():
-	fp2.write(line)
-fp.close()
-fp2.close()
+file = open("/Users/devpark/Documents/mini/tempContent1", "r")
+input_data = ""
+
+for line in file.readlines():
+	input_data += line
+content_length = len(input_data)
+file.close()
+json_data = json.loads(input_data)
+name = json_data.get('name')
+age = json_data.get('age')
+print(name, age)
