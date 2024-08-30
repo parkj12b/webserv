2fa3a85e02f4824fc1d9be9e8df02f3b76b24608
Ingyu Hwang <inghwang@cx1r1s3.42seoul.kr>
2024-08-30 16:16:46 +0900

    .

--- cgi-bin/upload/upload.php
+++ cgi-bin/upload/upload.php
@@ -31,13 +31,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         foreach ($_FILES['file_php']['name'] as $key => $name) {
             if ($_FILES['file_php']['error'][$key] == UPLOAD_ERR_OK) {
                 $tmp_name = $_FILES['file_php']['tmp_name'][$key];
-                $destination = '../../uploads/' . basename($name);
+                $destination = $uploadDir.basename($name);
 
                 // Move the uploaded file to the desired directory
-                if (move_uploaded_file($tmp_name, $destination)) {
-                    echo "StaTus: 302\r\n";
+                if (move_uploaded_file($tmp_name, $destination))
+                {
+                    echo "Status: 302\r\n";
                     echo "location: /upload_success.html\r\n";
-                } else {
+                }
+                else
+                {
                     echo "status: 400\r\n";
                 }
             } else {
