dc2781be03f3e04e84cbb4b08bf3283448d9cdff
Ingyu Hwang <inghwang@cx1r1s3.42seoul.kr>
2024-08-30 16:51:56 +0900

    upload php

--- cgi-bin/upload/upload.php
+++ cgi-bin/upload/upload.php
@@ -25,14 +25,28 @@
 
 // header_remove();  // 모든 헤더 제거
 $uploadDir = getenv("UPLOAD_PATH");
+$uploadedFiles = [];
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    if (isset($_FILES['file_php']) && is_array($_FILES['file_php']['name'])) {
-        foreach ($_FILES['file_php']['name'] as $key => $name) {
-            if ($_FILES['file_php']['error'][$key] == UPLOAD_ERR_OK) {
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+    if (isset($_FILES['file_php']) && is_array($_FILES['file_php']['name']))
+    {
+        foreach ($_FILES['file_php']['name'] as $key => $name)
+        {
+            if ($_FILES['file_php']['error'][$key] == UPLOAD_ERR_OK)
+            {
                 $tmp_name = $_FILES['file_php']['tmp_name'][$key];
                 $destination = $uploadDir.basename($name);
-
+                $fileExtension = pathinfo($name, PATHINFO_EXTENSION);
+                if ($fileExtension === 'py' || $fileExtension === 'php')
+                {
+                    foreach ($uploadedFiles as $file)
+                    {
+                        unlink($file);
+                    }
+                    echo "status: 400\r\n";
+                    exit;
+                }
                 // Move the uploaded file to the desired directory
                 if (move_uploaded_file($tmp_name, $destination))
                 {
@@ -41,13 +55,29 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 }
                 else
                 {
+                    foreach ($uploadedFiles as $file)
+                    {
+                        unlink($file);
+                    }
                     echo "status: 400\r\n";
                 }
-            } else {
+            }
+            else
+            {
+                foreach ($uploadedFiles as $file)
+                {
+                    unlink($file);
+                }
                 echo "status: 400\r\n";
             }
         }
-    } else {
+    }
+    else
+    {
+        foreach ($uploadedFiles as $file)
+        {
+            unlink($file);
+        }
         echo "status: 400\r\n";
         // exit ;
     }
